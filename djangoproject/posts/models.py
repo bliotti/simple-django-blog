b9d4c88dfd247aefc69c85295fb03d8751e220cc
Brian Liotti <bdliotti@gmail.com>
2018-09-26 10:39:22 -0400

    iitial commit

---
+++ djangoproject/posts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+
+
+class Posts(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
